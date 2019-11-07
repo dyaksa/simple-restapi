4470cef19d65baedd25d61967d86f19d491aa2c0
dyaksa <diasnour0395@gmail.com>
2019-11-07 23:38:05 +0700

    tambah method put

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const _ = require("lodash");
+const slug = require("slug");
 const Schema = mongoose.Schema;
+const {
+    check,
+    validationResult
+} = require("express-validator");
 
 mongoose.connect("mongodb://localhost:27017/wikiDB", {
     useNewUrlParser: true,
@@ -14,6 +20,7 @@ mongoose.connect("mongodb://localhost:27017/wikiDB", {
 
 const articlesSchema = Schema({
     _id: mongoose.Types.ObjectId,
+    slug: String,
     title: String,
     content: String
 });
@@ -26,27 +33,78 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(express.static("public"));
+app.use(express.json());
 app.set("view engine", "ejs");
 
-app.get("/articles", function (req, res) {
-    Article.find({}, function (err, articles) {
-        if (err) throw err;
-        res.send(articles);
-    });
-});
+app.route("/articles")
+    .get((req, res) => {
+        Article.find({}, function (err, articles) {
+            if (err) throw err;
+            res.send(articles);
+        });
+    }).post([
+        check("title").isLength({
+            min: 3
+        }),
+        check("content").isLength({
+            min: 10
+        })
+    ], (req, res) => {
+        let lowerTitle = _.lowerCase(req.body.title);
+        const errors = validationResult(req);
+        if (!errors.isEmpty) {
+            return res.status(422).json({
+                errors: errors.array()
+            });
+        } else {
+            const newArticle = new Article({
+                _id: new mongoose.Types.ObjectId,
+                slug: slug(lowerTitle),
+                title: req.body.title,
+                content: req.body.content
+            });
 
-app.post("/articles", function (req, res) {
-    const newArticle = new Article({
-        _id: new mongoose.Types.ObjectId,
-        title: req.body.title,
-        content: req.body.content
+            newArticle.save(function (err) {
+                if (err) throw err;
+                res.send("Successfully add article!");
+            })
+        }
+
+    }).delete((req, res) => {
+        Article.deleteMany((err) => {
+            if (err) throw err;
+            res.send("Successfully deleted all article");
+        });
     });
 
-    newArticle.save(function (err) {
-        if (err) throw err;
-        res.send("Successfully add article!");
-    })
-});
+//specific article
+app.route("/articles/:slug")
+    .get((req, res) => {
+        let articleSlug = _.toLower(req.params.slug);
+        Article.findOne({
+            slug: articleSlug
+        }, function (err, article) {
+            if (err) throw err;
+            if (article) {
+                res.send(article);
+            } else {
+                console.log(title);
+                res.send("Not found");
+            }
+        });
+    }).put((req, res) => {
+        let titleSlug = _.lowerCase(req.body.title);
+        Article.updateOne({
+            slug: _.toLower(req.params.slug)
+        }, {
+            slug: slug(titleSlug),
+            title: req.body.title,
+            content: req.body.content
+        }, function (err) {
+            if (err) throw err;
+            res.send("successfully updated data");
+        });
+    });
 
 app.listen(process.env.PORT || 3000, function () {
     console.log("connected");
